5f6d2241b4b333966ef66c3127fbc5946d5c0c01
patrick-king <33234942+patrick-king@users.noreply.github.com>
2022-07-21 16:15:16 -0400

    Add project files.

---
+++ MVCMoviesAPI/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using MVCMoviesAPI.Models;
+
+namespace MVCMoviesAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
